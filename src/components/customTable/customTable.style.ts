0629fb7d154e58c297d465ea9648a5e36972783a
under305 <underjoel30@gmail.com>
2020-05-05 01:37:10 -0500

    Tabla con estilos

--- src/components/customTable/customTable.style.ts
+++ src/components/customTable/customTable.style.ts
@@ -1,13 +1,31 @@
 import styled from "styled-components";
 
-export const TableWrapper = styled.table``;
+export const TableWrapper = styled.table`
+	margin: 10px;
+	
+	padding: 10px;
+	border-radius: 25px;
+	background: rgb(2,0,36);
+	background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 0%, rgba(9,9,121,1) 0%, rgba(60,46,224,0.5018382352941176) 0%, rgba(0,212,255,1) 100%);
+`;
+//background: rgba(255, 255, 255, .7);
+export const TableHeader = styled.thead`
+	background: rgba(234, 97, 97, .8);
+	color: #EBEBEB;
+	border-radius: 25px 25px 0px 0px;
+`;
 
-export const TableHeader = styled.thead``;
+export const TableH = styled.th`
+	
+`;
 
-export const TableH = styled.th``;
+export const TableBody = styled.tbody`
+	
+`;
 
-export const TableBody = styled.tbody``;
-
-export const TableRow = styled.tr``;
+export const TableRow = styled.tr`
+	background: rgba(255, 255, 255, .7);
+	border-radius: 25px 25px 0px 0px;
+`;
 
 export const TableData = styled.td``;
